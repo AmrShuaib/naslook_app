e8992ac7193a9ab93963bd96a3b6dcbea9e27632
3mro <jeddahh@gmail.com>
2025-08-17 09:27:06 +0300

    Initial commit: Naslook Flutter app

---
+++ lib/main.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false, // يشيل شعار DEBUG
+      title: 'Naslook App',
+      theme: ThemeData(primarySwatch: Colors.blue),
+      home: const HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text("Naslook App")),
+      body: const Center(
+        child: Text("Hello World 👋", style: TextStyle(fontSize: 26)),
+      ),
+    );
+  }
+}
